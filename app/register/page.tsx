a6b9b32ebcbaa610d2ccc729497008e221aedd25
Avinash Pise <avinashpise.tech@gmail.com>
2026-07-20 18:33:33 +0530

    Fix missing next/image import in register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
 import { AppCard, AppCardHeader, AppCardTitle, AppCardContent } from "@/components/ui/AppCard";
